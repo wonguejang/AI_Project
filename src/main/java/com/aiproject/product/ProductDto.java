6f6327db1800852276beaa9bcae598953bfd6748
wonguejang <https://github.com/wonguejang/project.git>
2025-09-14 12:44:33 +0900

    엔티티 끝, dto생성, 추가작업필요

--- src/main/java/com/aiproject/product/ProductDto.java
+++ src/main/java/com/aiproject/product/ProductDto.java
@@ -1,5 +1,26 @@
 package com.aiproject.product;
 
-public class ProductDto {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class ProductDto {
+	private Integer productIdx;
+	
+	private String productName;
+	
+	private Integer price;
+	
+	private String category;
+	
+	private Integer rating;
+	
+	public ProductDto(Product product) {
+		productIdx = product.getProductIdx();
+		productName = product.getProductName();
+		price = product.getPrice();
+		category = product.getCategory();
+		rating = product.getRating();
+	}
 }
--- src/main/java/com/aiproject/reply/ReplyDto.java
+++ src/main/java/com/aiproject/reply/ReplyDto.java
@@ -1,5 +1,25 @@
 package com.aiproject.reply;
 
-public class ReplyDto {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class ReplyDto {
+	private Integer rno;
+	
+	private String content;
+	
+	private String writer;
+	
+	private String writeDate;
+	
+	public ReplyDto(Reply reply) {
+		rno = reply.getRno();
+		content = reply.getContent();
+		writer = reply.getMember().getMemberEmail();
+		writeDate = reply.getWriteDate().toString();
+		writeDate = writeDate.replace("T", " ");
+		writeDate = writeDate.substring(0, writeDate.indexOf("."));
+	}
 }
