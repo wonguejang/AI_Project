e527e8abbea791e7b83ac99d70aca4a07221b322
wonguejang <https://github.com/wonguejang/project.git>
2025-09-15 14:59:41 +0900

    원규 수정중

--- src/main/java/com/aiproject/member/MemberController.java
+++ src/main/java/com/aiproject/member/MemberController.java
@@ -1,13 +1,21 @@
 package com.aiproject.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
+	@Autowired
+	MemberService mSvc;
+	
 	@GetMapping("/login")
 	public String loginForm(Model model) {
 		return "login";
@@ -16,6 +24,26 @@ public class MemberController {
 	@GetMapping("/signup")
 	public String signup(Model model) {
 		return "signup";
+	}
+	
+	@PostMapping("/signup_form")
+	public String signup(@Valid MemberCreateForm memberCreateForm, BindingResult br) {
+		if(br.hasErrors()) {
+			return "signup_form";
+		}
+
+		if(!memberCreateForm.getMemberPw1().equals(memberCreateForm.getMemberPw2())) {
+			br.rejectValue("userPw2", "passwordIncorect중x", "2개의 비밀번호가 일치하지 않음");
+			return "signup_form";
+		}
+		
+		String id = memberCreateForm.getMemberEmail();
+		String pw = memberCreateForm.getMemberPw1();
+		String name = memberCreateForm.getMemberName();
+		mSvc.create(id, pw, name);
+		
+		return "redirect:/main";
+		
 	}
 	
 	
